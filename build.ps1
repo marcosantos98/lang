1a8a891f6935749b81f12a9f88ba719852fafad1
marcosantos98 <marcomsantos98@gmail.com>
2025-01-07 01:56:59 +0000

    Add: Temporary test all with clang compilation

--- build.ps1
+++ build.ps1
@@ -1,10 +1,14 @@
 function fail_on_err {
 	param(
 		[string] $command,
-		[string] $label
+		[string] $label,
+		[bool] $newline = $true
 	)
+	Write-Host "> " -ForegroundColor Green -NoNewline
 	Write-Host $command
-	Write-Host ""
+	if ( $newline -eq "true" ) {
+	    Write-Host ""
+	}
 	Invoke-Expression $command
 	if ($LastExitCode -ne 0) {
 		Write-Host $label -ForegroundColor Red
@@ -18,6 +22,19 @@ if ( $args[0] -eq "rec") {
     }
     Get-ChildItem -Path "./tests/" -Filter "*.cpp" | ForEach-Object {
 	mv -Force $($_.FullName) "$($_.FullName).out"	
+    }
+} elseif ( $args[0] -eq "test") {
+    Write-Host "Transpile all tests" -ForegroundColor Red
+    Get-ChildItem -Path "./tests/" -Filter "*.lang" | ForEach-Object {
+	fail_on_err "./lang.exe $($_.FullName)" "Failed to compile $($_.FullName)" -newline $false
+    }
+    Write-Host "Compile all tests with clang++" -ForegroundColor Red
+    Get-ChildItem -Path "./tests/" -Filter "*.cpp" | ForEach-Object {
+	if ( $($_.Name) -eq "std.cpp") {
+	    Write-Host "Ignored std" -ForegroundColor Cyan
+	} else {
+	    fail_on_err "clang++ $($_.FullName)" "Failed to compile transpiled c++" -newline $false	
+	}
     }
 } else {
     $release = $false
